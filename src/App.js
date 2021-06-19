5ae104ef09914c9795dd8cba29cd730af1b43469
Shishir Thapa <shishirthapa1@gmail.com>
2021-06-19 22:39:44 +0545

    complete chat app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+// Components
+import Button from './components/Button';
+import Channel from './components/Channel';
+
+// Firebase deps
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAD0RGd3wfSTRbfTscK2FdgRL6BHqfBSEs",
+  authDomain: "ccrchat-e4996.firebaseapp.com",
+  projectId: "ccrchat-e4996",
+  storageBucket: "ccrchat-e4996.appspot.com",
+  messagingSenderId: "41928426673",
+  appId: "1:41928426673:web:9af732e0ce1414f3ca4e90"
+});
+
+const auth = firebase.auth();
+const db = firebase.firestore();
 
 function App() {
+  const [user, setUser] = useState(() => auth.currentUser);
+  const [initializing, setInitializing] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      user ? setUser(user) : setUser(null);
+    })
+    
+    if(initializing) setInitializing(false);
+
+    // Cleanup subscription
+    return unsubscribe;
+  }, [])
+
+  const signInWithGoogle = async () => {
+    // Retrieve Google provider object
+    const provider = new firebase.auth.GoogleAuthProvider();
+    // Set language to the default browser preference
+    auth.useDeviceLanguage();
+    // Start sign in process
+    try {
+      await auth.signInWithPopup(provider);
+    } catch(error) {
+      alert('Something went wrong. Try another account!!');
+    }
+
+  }
+
+  const signOut = async () => {
+    try {
+      await firebase.auth().signOut();
+    } catch(error) {
+      console.log(error.message);
+    }
+  }
+
+  if(initializing) return 'Loading...'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { user ? (
+          <>
+            <Button onClick={signOut}>Sign Out</Button>
+            <Channel user={user} db={db} />
+          </>
+        ) : (
+          <Button onClick={signInWithGoogle}>Sign in with Google</Button>
+        )}
     </div>
   );
 }
---
+++ src/components/Channel.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import Message from './Message';
+
+
+const Channel = ({ user = null, db = null}) => {
+    const {messages, setMessages} = useState([]);
+    const {newMessage, setNewMessage} = useState('');
+
+    const { uid, displayName, photoURL } = user;
+
+    useEffect(() => {
+        if(db) {
+            const unsubscribe = db.collection('messages')
+                .orderBy('createdAt')
+                .limit(100)
+                .onSnapshot(querySnapshot => {
+                    // Get all documents from collection - with IDs
+                    const data = querySnapshot.docs.map(doc => ({
+                        ...doc.data(),
+                        id: doc.id
+                    }));
+                    // Update State
+                    setMessages(data);
+                })
+            
+            // Detach listener
+            return unsubscribe;
+            
+        }
+    }, [db])
+
+    const handleOnChange = e => {
+        setNewMessage(e.target.value);
+    }
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+
+        if(db) {
+            db.collection('messages').add({
+                text: newMessage,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                uid,
+                displayName,
+                photoURL
+            })
+        }
+
+    }
+
+    return (
+        <>
+            <ul>
+                {messages?.map(message => {
+                    <li key={message.id}><Message {...message} /></li>
+                })}
+            </ul>
+            <form onSubmit={handleOnSubmit}>
+                <input
+                    type="text"
+                    value={newMessage}
+                    onChange={handleOnChange}
+                    placeholder="Type your message here..."
+                />
+                <button type="submit" disabled={!newMessage}>Send</button>
+            </form>
+        </>
+    )
+}
+
+export default Channel;
